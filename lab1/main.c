d545b93d2e02a62b717e29dccd7778f374cd8fba
Bartłomiej <bartlomiej.sikora@op.pl>
2022-10-02 15:37:46 +0200

    Modified main.c

--- lab1/main.c
+++ lab1/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
+#define #k %s
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
 			i++;
-			printf("%s",param);
+			printf("#k", toupper(param));
+			printf("#k", tolower(param));
+			printf("#k",param);
 		}else
 			putchar(format_string[i]);
 	}
