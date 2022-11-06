710f816ba3c639c4906dc91dedb1aca2a902447c
Bartłomiej Sikora <bartlomiej.sikora@op.pl>
2022-11-06 15:23:18 +0100

    Add #g algorithm

--- lab4/main.cs
+++ lab4/main.cs
@@ -6,8 +6,33 @@ namespace lab0
     class Hello {         
 	static void my_printf(string format_string, string param){
 		for(int i=0;i<format_string.Length;i++){
-			if((format_string[i] == '#') && (format_string[i+1] == 'k')){
-				Console.Write(param);
+			if((format_string[i] == '#') && (format_string[i+1] == 'g')){
+				
+				Int32 original = 0;
+				Int32 val = 0;
+				if(Int32.TryParse(param, out original)) {
+					val = original;
+					if(original < 0) {
+						val = Math.Abs(original);
+					}
+					Int32 reverse = 0;
+					while (val > 0)
+					{
+						Int32 r = val % 10;
+						reverse = (reverse*10) + r;
+						val = val / 10;
+					}
+					if(original < 0)
+					{
+						reverse = (reverse * (-1));
+					}
+					
+					Console.Write(reverse);
+				}
+				else {
+					Console.Write("err");
+				}
+
 				i++;
 			}else{
 				Console.Write(format_string[i]);
