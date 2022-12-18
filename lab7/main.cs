3e518023030e181a33f0459e00a2f280c82e7d25
Leno <bartlomiej.sikora@op.pl>
2022-12-18 15:19:12 +0100

    commit_1

--- lab7/main.cs
+++ lab7/main.cs
@@ -9,25 +9,13 @@ namespace lab0
 		for(int i=0;i<format_string.Length;i++){
 
 			char c = format_string[i];
-
 			if (c == '#' && i + 1 < format_string.Length && format_string[i + 1] == 'j')
 			{
-				i += 2; // Przeskakujemy dwa znaki ('#' i 'j')
-				int value = (int)param[0]; // Pobieramy wartość liczby szesnastkowej z listy argumentów
-				Console.Write(hex(value)); // Drukujemy zamienioną liczbę szesnastkową
-				param = param.Skip(1).ToArray(); // Usuwamy pierwszy element z listy argumentów
+				i += 2;
+				int value = Convert.ToInt32(param);
+				Console.Write(hex(value));
 			}
-			// else if (c == '%') // Jeśli napotkamy znak '%', to sprawdzamy, czy następny znak to 'x'
-			// {
-			//     i++; // Przeskakujemy znak '%'
-			//     if (i < format_string.Length && format_string[i] == 'x')
-			//     {
-			//         int value = (int)args[0]; // Pobieramy wartość liczby szesnastkowej z listy argumentów
-			//         Console.Write(hex(value)); // Drukujemy zamienioną liczbę szesnastkową
-			//         args = args.Skip(1).ToArray(); // Usuwamy pierwszy element z listy argumentów
-			//     }
-			// }
-			else // Jeśli to zwykły znak, to drukujemy go bez zmian
+			else
 			{
 				Console.Write(c);
 			}
@@ -37,12 +25,10 @@ namespace lab0
 
 		public static string hex(int value)
         {
-            // Zamieniamy liczbę szesnastkową na string i iterujemy po każdym znaku
             string hex = value.ToString("X");
             for (int i = 0; i < hex.Length; i++)
             {
                 char c = hex[i];
-                // Zamieniamy cyfry a-f na g-l
                 if (c >= 'a' && c <= 'f')
                 {
                     c = (char)(c + 'g' - 'a');
