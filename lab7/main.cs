009b548e842ded63df71c654a389b66fd58118cb
Leno <bartlomiej.sikora@op.pl>
2022-12-18 14:56:08 +0100

    modiffied main.cs

--- lab7/main.cs
+++ lab7/main.cs
@@ -7,78 +7,59 @@ namespace lab0
     class Hello {         
 	static void my_printf(string format_string, string param){
 		for(int i=0;i<format_string.Length;i++){
-			
-			if(format_string[i] == '#' && format_string[i+i] == '.'){
 
-				i++;
-				string strNumber = "";
-				int x=0;
+			char c = format_string[i];
 
-				for(int j=i+1; j<format_string.Length;j++)
-				{
-					if(int.TryParse(format_string[j].ToString(), out x))
-					{
-						strNumber += format_string[j];
-						i++;
-					}
-					else 
-					{
-						break;	
-					}
-				}
-
-				if(format_string[i+1] == 'g' && int.TryParse(strNumber, out x)){
-					string temp="";
-					string firstChar;
-					int num = 0; 
-					//string new = str.Substring(0, 1); // "1"
-
-					for (int k=0; k<param.Length;k++)
-					{
-						string p = param;
-						firstChar = p.Substring(0, k);
-
-						if (int.TryParse(firstChar, out num))
-						{
-							num = ((num *9+1)%10);			
-						}
-
-						temp = temp + num.ToString();
-					}
-
-					if(x >= param.Length)
-					{
-						string result = new StringBuilder().Insert(0, " ", x-param.Length).ToString();
-						result += temp;
-						Console.Write(result);
-					}
-					else if(x < param.Length)
-					{
-						Console.Write(temp.Substring(0, x));
-					}
-					else if(x == 0)
-					{
-						Console.Write(temp);
-					}	
-				}
-
-				i++;
+			if (c == '#' && i + 1 < format_string.Length && format_string[i + 1] == 'j')
+			{
+				i += 2; // Przeskakujemy dwa znaki ('#' i 'j')
+				int value = (int)param[0]; // Pobieramy wartość liczby szesnastkowej z listy argumentów
+				Console.Write(hex(value)); // Drukujemy zamienioną liczbę szesnastkową
+				param = param.Skip(1).ToArray(); // Usuwamy pierwszy element z listy argumentów
 			}
-			else{
-				Console.Write(format_string[i]);
+			// else if (c == '%') // Jeśli napotkamy znak '%', to sprawdzamy, czy następny znak to 'x'
+			// {
+			//     i++; // Przeskakujemy znak '%'
+			//     if (i < format_string.Length && format_string[i] == 'x')
+			//     {
+			//         int value = (int)args[0]; // Pobieramy wartość liczby szesnastkowej z listy argumentów
+			//         Console.Write(hex(value)); // Drukujemy zamienioną liczbę szesnastkową
+			//         args = args.Skip(1).ToArray(); // Usuwamy pierwszy element z listy argumentów
+			//     }
+			// }
+			else // Jeśli to zwykły znak, to drukujemy go bez zmian
+			{
+				Console.Write(c);
 			}
+            }
+			Console.Write("\n");
+        }
 
-		}
-		Console.Write("\n");
-	}
+		public static string hex(int value)
+        {
+            // Zamieniamy liczbę szesnastkową na string i iterujemy po każdym znaku
+            string hex = value.ToString("X");
+            for (int i = 0; i < hex.Length; i++)
+            {
+                char c = hex[i];
+                // Zamieniamy cyfry a-f na g-l
+                if (c >= 'a' && c <= 'f')
+                {
+                    c = (char)(c + 'g' - 'a');
+                }
+                hex = hex.Substring(0, i) + c + hex.Substring(i + 1);
+            }
+            return hex;
+        }
+
         static void Main(string[] args)
         {
-	    string format_string;
-	    while ((format_string = Console.ReadLine()) != null)
-	    {
-		string param = Console.ReadLine();
-		my_printf(format_string,param);
-	    }
+	    	string format_string;
+	   		while ((format_string = Console.ReadLine()) != null)
+	    	{
+				string param = Console.ReadLine();
+				my_printf(format_string,param);
+	   		}
         }
     }
 }
